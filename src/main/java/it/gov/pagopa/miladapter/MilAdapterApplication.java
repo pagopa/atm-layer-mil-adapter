ff92d91a2658964552abc590032791801c839267
Cocuzzoli, Andrea <andrea.cocuzzoli@nttdata.com>
2023-10-09 14:47:28 +0200

    Initial commit

---
+++ src/main/java/it/gov/pagopa/miladapter/MilAdapterApplication.java
@@ -0,0 +1,13 @@
+package it.gov.pagopa.miladapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilAdapterApplication.class, args);
+	}
+
+}
