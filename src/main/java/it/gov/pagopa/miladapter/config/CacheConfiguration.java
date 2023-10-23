a26a8744ea18fb3b576a2fb2d4fb047f5a0516cb
USERSAD\evalenti <emanuele.valentini@nttdata.com>
2023-10-23 11:12:04 +0200

    Remove Space Test pull request

--- src/main/java/it/gov/pagopa/miladapter/config/CacheConfiguration.java
+++ src/main/java/it/gov/pagopa/miladapter/config/CacheConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CacheConfiguration {
 
-
     @Autowired
     CacheConfigurationProperties cacheConfigurationProperties;
 
