23e3e36b4f5d92c390a9bfee51d967ffda813ee5
AndreaC <94940911+candreac@users.noreply.github.com>
2023-10-30 08:42:50 +0100

    HTTP Exception Management (#12)

--- src/main/java/it/gov/pagopa/miladapter/services/GenericRestService.java
+++ src/main/java/it/gov/pagopa/miladapter/services/GenericRestService.java
@@ -1,11 +1,15 @@
 package it.gov.pagopa.miladapter.services;
 
+import camundajar.impl.com.google.gson.JsonObject;
 import it.gov.pagopa.miladapter.enums.HttpVariablesEnum;
 import it.gov.pagopa.miladapter.model.HTTPConfiguration;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -14,9 +18,24 @@ public interface GenericRestService {
 
     default VariableMap executeRestCall(HTTPConfiguration configuration) {
         this.injectAuthToken(configuration);
-        ResponseEntity<String> response = this.getRestTemplate()
-                .exchange(this.prepareUri(configuration), configuration.getHttpMethod(), this.buildHttpEntity(configuration), String.class);
+        ResponseEntity<String> response;
+        try {
+            response = this.getRestTemplate()
+                    .exchange(this.prepareUri(configuration), configuration.getHttpMethod(), this.buildHttpEntity(configuration), String.class);
+        } catch (HttpClientErrorException e) {
+            response = new ResponseEntity<>(e.getResponseBodyAsString(), e.getStatusCode());
+        } catch (HttpServerErrorException e) {
+            response = new ResponseEntity<>(e.getResponseBodyAsString(), e.getStatusCode());
+            throw e;
+        } catch (Exception e) {
+            response = new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            throw e;
+        }
+
         VariableMap output = Variables.createVariables();
+        if(response.getBody() == null){
+            response = new ResponseEntity<>(new JsonObject().toString(),response.getStatusCode());
+        }
         output.putValue(HttpVariablesEnum.RESPONSE.getValue(), response.getBody());
         output.putValue(HttpVariablesEnum.STATUS_CODE.getValue(), response.getStatusCode().value());
         return output;
