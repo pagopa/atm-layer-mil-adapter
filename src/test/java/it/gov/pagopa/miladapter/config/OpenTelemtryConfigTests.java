c06a8bfbc0cf9f77b67982af05e6aa86f4ef52ab
Cocuzzoli, Andrea <andrea.cocuzzoli@nttdata.com>
2024-01-28 16:17:31 +0100

    added coverage

---
+++ src/test/java/it/gov/pagopa/miladapter/config/OpenTelemtryConfigTests.java
@@ -0,0 +1,17 @@
+package it.gov.pagopa.miladapter.config;
+
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+class OpenTelemtryConfigTests {
+    @InjectMocks
+    OpenTelemetryConfig openTelemetryConfig = new OpenTelemetryConfig();
+
+    @Test
+    void customPoolTest() {
+        OtlpGrpcSpanExporter otlpGrpcSpanExporter = openTelemetryConfig.otlpHttpSpanExporter("http://provaUrl");
+        assert (otlpGrpcSpanExporter != null);
+
+    }
+}
