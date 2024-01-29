cf364923da3affe1d0434b284c12e938b9c9187f
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-01-29 10:31:31 +0100

    integrating async behaviour

--- src/main/java/it/gov/pagopa/miladapter/engine/task/impl/IDPayRestTaskHandler.java
+++ src/main/java/it/gov/pagopa/miladapter/engine/task/impl/IDPayRestTaskHandler.java
@@ -10,6 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -22,6 +25,29 @@ public class IDPayRestTaskHandler implements RestExternalTaskHandler {
     IDPayRestService idPayRestService;
     @Autowired
     RestConfigurationProperties restConfigurationProperties;
+    @Value("${camunda.bpm.client.max-tasks}")
+    private int maxTasksConfig;
+    @Autowired
+    @Qualifier("#{adapter-pool-configuration.rest.name}")
+    private TaskExecutor taskRestExecutor;
+    @Autowired
+    @Qualifier("#{adapter-pool-configuration.completion.name}")
+    private TaskExecutor taskComplExecutor;
+
+    @Override
+    public TaskExecutor getTaskRestExecutor() {
+        return this.taskRestExecutor;
+    }
+
+    @Override
+    public TaskExecutor getTaskComplExecutor() {
+        return this.taskComplExecutor;
+    }
+
+    @Override
+    public int getMaxTasks() {
+        return this.maxTasksConfig;
+    }
 
     @Override
     public Logger getLogger() {
