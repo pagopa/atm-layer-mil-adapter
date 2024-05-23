0538335796fb71bc9e0cc5195dae7c549f51158e
GiacomoB <86237971+GiacomoBrancazi@users.noreply.github.com>
2024-05-23 16:03:22 +0200

    Uat (#72)
    Co-authored-by: Simone Munao <simone.munao@nttdata.com>
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: SimoneMiccoli1 <146010879+SimoneMiccoli1@users.noreply.github.com>
    Co-authored-by: SMANUM <125576365+SMANUM@users.noreply.github.com>
    Co-authored-by: Cocuzzoli, Andrea <andrea.cocuzzoli@nttdata.com>
    Co-authored-by: LuciaM1 <144435187+LuciaM1@users.noreply.github.com>
    Co-authored-by: Elis <elis.kina@nttdata.com>
    Co-authored-by: USERSAD\evalenti <emanuele.valentini@nttdata.com>
    Co-authored-by: EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
    Co-authored-by: ElisKina-dev <144669238+ElisKina-dev@users.noreply.github.com>
    Co-authored-by: GabrieleMaiocchiFilippo <fmaiocch@emeal.nttdata.com>
    Co-authored-by: Escarsel <eloisa.scarsella@nttdata.com>
    Co-authored-by: LuciaM1 <lucia.mazzocchi@nttdata.com>
    Co-authored-by: adrrss <147711350+adrrss@users.noreply.github.com>
    Co-authored-by: adrrossi <adriano.rossi@nttdata.com>

--- src/main/java/it/gov/pagopa/miladapter/services/impl/IDPayRestServiceImpl.java
+++ src/main/java/it/gov/pagopa/miladapter/services/impl/IDPayRestServiceImpl.java
@@ -1,31 +1,16 @@
 package it.gov.pagopa.miladapter.services.impl;
 
-import io.opentelemetry.api.trace.Tracer;
 import it.gov.pagopa.miladapter.model.Configuration;
-import it.gov.pagopa.miladapter.properties.RestConfigurationProperties;
-import it.gov.pagopa.miladapter.resttemplate.RestTemplateGenerator;
 import it.gov.pagopa.miladapter.services.IDPayRestService;
-import it.gov.pagopa.miladapter.services.TokenService;
 import it.gov.pagopa.miladapter.util.HttpRequestUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
 
 @Service
 @Slf4j
-public class IDPayRestServiceImpl implements IDPayRestService {
-    @Autowired
-    RestConfigurationProperties restConfigurationProperties;
-    @Autowired
-    RestTemplateGenerator restTemplateGenerator;
-    @Autowired
-    TokenService tokenService;
-    @Autowired
-    Tracer tracer;
+public class IDPayRestServiceImpl extends GenericRestServiceAbstract implements IDPayRestService {
 
     @Override
     public void injectAuthToken(Configuration configuration) {
@@ -37,28 +22,4 @@ public class IDPayRestServiceImpl implements IDPayRestService {
         return HttpRequestUtils.buildURI(restConfigurationProperties.getIdPayBasePath(), configuration.getEndpoint(), configuration.getPathParams());
     }
 
-    @Override
-    public Logger getLogger() {
-        return log;
-    }
-
-    @Override
-    public Tracer getTracer() {
-        return this.tracer;
-    }
-
-    @Override
-    public RestConfigurationProperties getRestConfigurationProperties() {
-        return this.restConfigurationProperties;
-    }
-
-    @Override
-    public RestTemplateGenerator getRestTemplateGenerator() {
-        return this.restTemplateGenerator;
-    }
-
-    @Override
-    public HttpEntity<String> buildHttpEntity(Configuration configuration) {
-        return HttpRequestUtils.buildHttpEntity(configuration.getBody(), configuration.getHeaders());
-    }
 }
