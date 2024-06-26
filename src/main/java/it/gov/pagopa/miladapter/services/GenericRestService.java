a30f6d07450bd5561b17460b9b19e66b8f9e120a
adrrossi <adriano.rossi@nttdata.com>
2024-06-26 18:57:45 +0200

    add log

--- src/main/java/it/gov/pagopa/miladapter/services/GenericRestService.java
+++ src/main/java/it/gov/pagopa/miladapter/services/GenericRestService.java
@@ -35,7 +35,7 @@ public interface GenericRestService  {
 
     default VariableMap executeRestCall(Configuration configuration) {
         ResponseEntity<String> response;
-        getLogger().info("Start span transactionId: ", configuration.getHeaders().get("TransactionId"));
+        getLogger().info("Start span requestId: ", configuration.getHeaders().get("requestId"));
         SpanBuilder spanBuilder = this.spanBuilder(configuration);
         Span serviceSpan = spanBuilder.startSpan();
 
@@ -51,11 +51,11 @@ public interface GenericRestService  {
                     Thread.currentThread().interrupt();
                 }
             }
-            getLogger().info("End span transactionId: ", configuration.getHeaders().get("TransactionId"));
-            getLogger().info("Start get token transactionId: ", configuration.getHeaders().get("TransactionId"));
+            getLogger().info("End span requestId: ", configuration.getHeaders().get("requestId"));
+            getLogger().info("Start get token requestId: ", configuration.getHeaders().get("requestId"));
             this.injectAuthToken(configuration);
-            getLogger().info("End get token transactionId: ", configuration.getHeaders().get("TransactionId"));
-            getLogger().info("Start create call request transactionId: ", configuration.getHeaders().get("TransactionId"));
+            getLogger().info("End get token requestId: ", configuration.getHeaders().get("requestId"));
+            getLogger().info("Start create call request requestId: ", configuration.getHeaders().get("requestId"));
             URI url = this.prepareUri(configuration);
             HttpEntity<String> entity = this.buildHttpEntity(configuration);
 //            serviceSpan.setAttribute(SemanticAttributes.HTTP_METHOD, configuration.getHttpMethod().name());
@@ -64,7 +64,7 @@ public interface GenericRestService  {
 //                serviceSpan.setAttribute("http.body", entity.getBody());
 //            }
 //            serviceSpan.setAttribute("http.headers", entity.getHeaders().toString());
-            getLogger().info("Stop create call request transactionId: ", configuration.getHeaders().get("TransactionId"));
+            getLogger().info("Stop create call request requestId: ", configuration.getHeaders().get("requestId"));
 
 
              Span externalCallSpan = getTracer().spanBuilder("MIL external call")
@@ -77,11 +77,11 @@ public interface GenericRestService  {
                     externalCallSpan.setAttribute("http.body", entity.getBody());
                 }
                 externalCallSpan.setAttribute("http.headers", entity.getHeaders().toString());
-                getLogger().info("Start rest call transactionId: ", configuration.getHeaders().get("TransactionId"));
+                getLogger().info("Start rest call requestId: ", configuration.getHeaders().get("requestId"));
 
                 response = this.getRestTemplate(configuration)
                         .exchange(url, configuration.getHttpMethod(), entity, String.class);
-                getLogger().info("End rest call transactionId: ", configuration.getHeaders().get("TransactionId"));
+                getLogger().info("End rest call requestId: ", configuration.getHeaders().get("requestId"));
             } finally {
                 externalCallSpan.end();
             }
@@ -99,7 +99,7 @@ public interface GenericRestService  {
             getLogger().error("Exception in HTTP request: ", e);
             response = new ResponseEntity<>(new JsonObject().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        getLogger().info("Start mapping response transactionId: ", configuration.getHeaders().get("TransactionId"));
+        getLogger().info("Start mapping response requestId: ", configuration.getHeaders().get("requestId"));
         VariableMap output = Variables.createVariables();
         if (response.getBody() == null) {
             response = new ResponseEntity<>(new JsonObject().toString(), response.getStatusCode());
@@ -121,7 +121,7 @@ public interface GenericRestService  {
         SpinJsonNode headersJsonNode = JSON(response.getHeaders());
         output.putValue(HttpVariablesEnum.RESPONSE_HEADERS.getValue(), headersJsonNode.toString());
         serviceSpan.end();
-        getLogger().info("Stop mapping response transactionId: ", configuration.getHeaders().get("TransactionId"));
+        getLogger().info("Stop mapping response requestId: ", configuration.getHeaders().get("requestId"));
         return output;
     }
 
