f9de7f09e3af2ce0fe42fb1ee4100b1fe341eb79
adrrossi <adriano.rossi@nttdata.com>
2024-06-28 12:14:37 +0200

    add callback camunda wait message

---
+++ src/main/java/it/gov/pagopa/miladapter/dto/CamundaWaitMessage.java
@@ -0,0 +1,17 @@
+package it.gov.pagopa.miladapter.dto;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class CamundaWaitMessage {
+
+    private String messageName;
+
+    private String processInstanceId;
+
+    private Map<String, ProcessVariable> processVariables;
+
+
+}
