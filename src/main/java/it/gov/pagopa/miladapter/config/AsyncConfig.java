87af43a8a79e7178c17d961ded50fb6b075c210d
EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
2024-07-01 14:34:59 +0200

    Testmessage (#93)
    Co-authored-by: adrrossi <adriano.rossi@nttdata.com>

---
+++ src/main/java/it/gov/pagopa/miladapter/config/AsyncConfig.java
@@ -0,0 +1,21 @@
+package it.gov.pagopa.miladapter.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncThread-");
+        executor.initialize();
+        return executor;
+    }
+}
