f2a5cfc9e654105af3cf3356fdef95d3493bf64b
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-07-01 15:22:12 +0200

    adding span context, fixing execute external

--- src/main/java/it/gov/pagopa/miladapter/services/impl/ExternalCallServiceImpl.java
+++ src/main/java/it/gov/pagopa/miladapter/services/impl/ExternalCallServiceImpl.java
@@ -1,7 +1,10 @@
 package it.gov.pagopa.miladapter.services.impl;
 
 import camundajar.impl.com.google.gson.JsonObject;
+import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 import it.gov.pagopa.miladapter.dto.CamundaWaitMessage;
 import it.gov.pagopa.miladapter.dto.ProcessVariable;
 import it.gov.pagopa.miladapter.enums.HttpVariablesEnum;
@@ -23,9 +26,12 @@ import org.camunda.spin.json.SpinJsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -100,15 +106,37 @@ public class ExternalCallServiceImpl extends GenericRestExternalServiceAbstract 
     }
 
     private  VariableMap callExternalService(Configuration configuration) {
-        HttpRequestUtils.buildHttpEntity(configuration.getBody(), configuration.getHeaders());
         ResponseEntity<String> response;
-        URI url = this.prepareUri(configuration);
-        HttpEntity<String> entity = HttpRequestUtils.buildHttpEntity(configuration.getBody(), configuration.getHeaders());
-        response = this.getRestTemplate(configuration).exchange(url, configuration.getHttpMethod(), entity, String.class);
         VariableMap output = Variables.createVariables();
-        if (response.getBody() == null) {
-            response = new ResponseEntity<>(new JsonObject().toString(), response.getStatusCode());
+        SpanBuilder spanBuilder = this.spanBuilder(configuration);
+        Span serviceSpan = spanBuilder.startSpan();
+        try (Scope scope = serviceSpan.makeCurrent()){
+            URI url = this.prepareUri(configuration);
+            HttpEntity<String> entity = HttpRequestUtils.buildHttpEntity(configuration.getBody(), configuration.getHeaders());
+            serviceSpan.setAttribute(SemanticAttributes.HTTP_METHOD, configuration.getHttpMethod().name());
+            serviceSpan.setAttribute(SemanticAttributes.HTTP_URL, url.toString());
+            if (entity.hasBody()) {
+                serviceSpan.setAttribute("http.body", entity.getBody());
+            }
+            serviceSpan.setAttribute("http.headers", entity.getHeaders().toString());
+            response = this.getRestTemplate(configuration).exchange(url, configuration.getHttpMethod(), entity, String.class);
+            if (response.getBody() == null) {
+                response = new ResponseEntity<>(new JsonObject().toString(), response.getStatusCode());
+            }
+        } catch (HttpClientErrorException | HttpServerErrorException e) {
+            getLogger().error("Exception in HTTP request: {}", e);
+            response = new ResponseEntity<>(new JsonObject().toString(), e.getStatusCode());
+            serviceSpan.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, e.getStatusCode().value());
+            serviceSpan.setAttribute("http.response.body", e.getResponseBodyAsString());
+        } catch (Exception e) {
+            getLogger().error("Exception in HTTP request: {}", e);
+            response = new ResponseEntity<>(new JsonObject().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        serviceSpan.setAttribute(SemanticAttributes.HTTP_STATUS_CODE, response.getStatusCode().value());
+        serviceSpan.setAttribute("http.response.body", response.getBody());
+        serviceSpan.setAttribute("http.response.headers", response.getHeaders().toString());
+
         JsonValue jsonValue;
         if (StringUtils.isNotBlank(response.getBody())
                 && response.getStatusCode() != null
@@ -121,6 +149,7 @@ public class ExternalCallServiceImpl extends GenericRestExternalServiceAbstract 
         output.putValue(HttpVariablesEnum.STATUS_CODE.getValue(), response.getStatusCode().value());
         SpinJsonNode headersJsonNode = JSON(response.getHeaders());
         output.putValue(HttpVariablesEnum.RESPONSE_HEADERS.getValue(), headersJsonNode.toString());
+        serviceSpan.end();
         return output;
     }
 
