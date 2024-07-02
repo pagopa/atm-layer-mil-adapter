975be2502214c1e2176a5322841878c35e1bba2e
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-07-02 10:42:59 +0200

    separating external call spans

--- src/main/java/it/gov/pagopa/miladapter/controller/ExternalCallsController.java
+++ src/main/java/it/gov/pagopa/miladapter/controller/ExternalCallsController.java
@@ -1,25 +1,18 @@
 package it.gov.pagopa.miladapter.controller;
 
-import it.gov.pagopa.miladapter.dto.CamundaWaitMessage;
-import it.gov.pagopa.miladapter.dto.ProcessVariable;
 import it.gov.pagopa.miladapter.engine.task.impl.MILRestServiceHandler;
-import it.gov.pagopa.miladapter.services.CallbackCamundaService;
 import it.gov.pagopa.miladapter.services.ExternalCallService;
 import it.gov.pagopa.miladapter.services.MILRestExternalService;
 import lombok.extern.slf4j.Slf4j;
-import org.camunda.bpm.engine.variable.VariableMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/externalcall")
@@ -39,10 +32,9 @@ public class ExternalCallsController {
     public ResponseEntity<String> externalcall(@RequestBody Map<String, Object> body) {
         // Log before starting async method
         log.info("Starting async execution");
-        externalCallService.executeAsyncTask(body);
-        // Log after starting async method
-        log.info("Async execution started");
-
+            externalCallService.executeAsyncTask(body);
+            // Log after starting async method
+            log.info("Async execution started");
         return new ResponseEntity<>("Headers processed successfully", HttpStatus.OK);
     }
 
